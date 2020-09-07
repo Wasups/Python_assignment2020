8f3a4bd6a898a283dc08af84ec24c2aae2f4fac1
Wasups <67409403+Wasups@users.noreply.github.com>
2020-09-07 23:16:53 +0800

    Web_crawl
    uploading web crawl file

--- Web_crawl.py
+++ Web_crawl.py
@@ -1,1 +1,33 @@
+import scrapy
 
+
+class TestCrawler2Spider(scrapy.Spider):
+    name = 'test_crawler2'
+    allowed_domains = ['172.18.58.238']
+    start_urls = ['http://172.18.58.238/index.php']
+    custom_settings = {
+        'DEPTH_LIMIT': 4
+    }
+
+    def parse(self, response):
+        css_selector = 'img'
+
+        #Crawled all website URL links
+        links = response.xpath("//a/@href").extract()
+        for link in links:
+            if link:
+
+                yield scrapy.Request(
+                    response.urljoin(link),
+                    callback=self.parse
+                )
+
+        #Crawled Image Links
+        for x in response.css(css_selector):
+            newsel='@src'
+            url = response.url
+            yield{
+                'Image Link': url+x.xpath(newsel).extract_first(),
+            }
+
+#Test to show that our team members can collaborate here!
